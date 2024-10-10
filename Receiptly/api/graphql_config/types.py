362c866ea531cbcbf82739f4714574f6c9ee02ff
Samyar <samyar1380@gmail.com>
2024-10-10 22:03:00 +0330

    Order info
    - added extra information for each receipt, showing the list of products selected for that receipt along with their count, effort and cost per unit

--- Receiptly/api/graphql_config/types.py
+++ Receiptly/api/graphql_config/types.py
@@ -2,7 +2,7 @@ import strawberry_django as sd
 from strawberry import auto
 from django.contrib.auth.models import User
 from .. import models
-from typing import List
+import strawberry
 
 
 @sd.type(model=User)
@@ -22,6 +22,14 @@ class ProductType:
     effort: auto
 
 
+@strawberry.type
+class ReceiptProductInfoType:
+    title: str
+    cost_per_unit: int
+    effort: float
+    count: int
+
+
 @sd.type(model=models.Receipt)
 class ReceiptType:
     user: "UserType"
@@ -33,3 +41,18 @@ class ReceiptType:
     deadline_date: auto
     deadline_notice: auto
     state: auto
+
+    # TODO: Consider redoing it using Strawberry's m2m support using typing.List
+    @strawberry.field
+    def products(self) -> list[ReceiptProductInfoType]:
+        products_list = []
+        for order in self.orderinfo_set.all():
+            products_list.append(
+                ReceiptProductInfoType(
+                    title=order.product.title,
+                    cost_per_unit=order.product.cost_per_unit,
+                    effort=order.product.effort,
+                    count=order.product_count,
+                )
+            )
+        return products_list
