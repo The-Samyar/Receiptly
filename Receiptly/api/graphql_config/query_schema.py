bf6d1e265a081666782dc0ef29bd6186b8cb3e42
Samyar <samyar1380@gmail.com>
2024-10-12 13:13:33 +0330

    Query based on ID
    - Users, Products, and Receipts can be queried based on ID

---
+++ Receiptly/api/graphql_config/query_schema.py
@@ -0,0 +1,36 @@
+import strawberry
+import strawberry_django as sd
+from .output_types import ProductType, UserType, ReceiptType
+from .. import models
+from django.contrib.auth.models import User
+from typing import Optional, List
+
+
+@strawberry.type
+class Query:
+    # --------------- User queries ---------------
+    # Single user based on ID
+    @strawberry.field
+    def user(self, user_id: int = None) -> UserType:
+        return User.objects.get(id=user_id)
+
+    # A list of all users
+    users: list[UserType] = sd.field()
+
+    # --------------- Products queries ---------------
+    # Single product based on ID
+    @strawberry.field
+    def product(self, product_id: int = None) -> ProductType:
+        return models.Product.objects.get(id=product_id)
+
+    # A list of all products
+    products: list[ProductType] = sd.field()
+
+    # --------------- Receipts queries ---------------
+    # Single receipt based on ID
+    @strawberry.field
+    def receipt(self, receipt_id: int = None) -> ReceiptType:
+        return models.Receipt.objects.get(id=receipt_id)
+
+    # A list of all receipts
+    receipts: list[ReceiptType] = sd.field()
