cec06c5f91b7c8a55b17bccafc9f999b8551aa78
ramin709 <79803446+ramin709@users.noreply.github.com>
2024-12-04 17:09:48 +0330

    Complete step two and step three
    - Create step three and fully implement it.
    - Add buttons to navigate through the steps properly.
    - Prevent the user to add products if they already exist
    - Display the latest updates on products each time you navigate into step two.

--- Receiptly/receiptly-front/src/Components/Form/FormStepTwo/FormStepTwo.jsx
+++ Receiptly/receiptly-front/src/Components/Form/FormStepTwo/FormStepTwo.jsx
@@ -15,18 +15,36 @@ export const FormStepTwo = ({ Receipt }) => {
   const [SelectedProducts, setSelectedProducts] = useState([]);
 
   const { loading, error, data } = useQuery(GET_PRODUCTS)
-  /* const {Data} = useFormContext();
-  console.log(Data) */
+  const {Data, goToStep, Step, updateData} = useFormContext();
+  console.log(Step)
 
-  console.log(SelectedProducts)
-  console.log(Products)
+  /* console.log(SelectedProducts)
+  console.log(Products) */
 
   useEffect(() => {
-    console.log(data)
-
+    /* console.log(data) */
     data?.products && setAvailableProducts(data.products)
-
   }, [data])
+
+  useEffect(() => {
+    if(Data.products){
+      setProducts(Data.products)
+    }
+  }, [Data])
+
+  const isProductExists = (product) => {
+    /* console.log(product)
+    console.log(Products) */
+    const existingProductIds = Products.map((product) => product.productId || product.id);
+    const result = existingProductIds.find(singleProduct => singleProduct === product.id)
+    console.log(result)
+    return result;
+  }
+
+  const changeStep = (step) => {
+    updateData({products: Products})
+    goToStep(step)
+  }
 
   const handleOnClick = (id, action) => {
 
@@ -53,9 +71,11 @@ export const FormStepTwo = ({ Receipt }) => {
     const existingProductIds = Products.map((product) => product.productId || product.id);
 
     // Filter SelectedProducts to only include new products not in Products
-    const newProducts = SelectedProducts.filter(
+    const filteredProducts = SelectedProducts.filter(
       (product) => !existingProductIds.includes(product.id)
     );
+
+    const newProducts = filteredProducts.map(product => ({ ...product, count: 1 }))
 
     // Add new products to Products and clear SelectedProducts
     setProducts((prev) => [...prev, ...newProducts]);
@@ -88,78 +108,28 @@ export const FormStepTwo = ({ Receipt }) => {
 
 
   return (
-    <div className={styles.StepTwoContainer}>
-      <FaCircleArrowLeft className={styles.AddIcon} onClick={() => addProductsToReceipt()} />
-      <div className={styles.receiptProductsList}>
-        <div className={styles.tableContainer}>
-          <table className={styles.table}>
-            <thead>
-              <th>Action</th>
-              <th>Product Name</th>
-              <th>Labor</th>
-              <th>Price</th>
-              <th>Add product</th>
-            </thead>
-
-            <tbody>
-              {
-                Products.map((product) => (
-                  product.count !== 0 ?
-                    <tr>
-                      <td>
-                        <div className={styles.td}>
-                          <IoCloseCircleSharp onClick={deleteProduct} id={product.productId || product.id} style={{ color: 'red', cursor: 'pointer', width: '20px', height: '20px' }} />
-                        </div>
-                      </td>
-                      <td>
-                        <div className={styles.tdContent}>
-                          {product.title}
-                        </div>
-                      </td>
-                      <td>
-                        <div className={styles.tdContent}>
-                          {product.effort} hours
-                        </div>
-                      </td>
-                      <td>
-                        <div className={styles.tdContent}>
-                          ${product.costPerUnit}
-                        </div>
-                      </td>
-                      <td>
-                        <div className={styles.tdContent}>
-                          <FaMinus className={styles.tdContentIcon} onClick={() => handleOnClick(product.productId, 'delete')} />
-                          <input type="text" /* readOnly={!edit} */ value={product.count} name="" id="" className={styles.inputTable} />
-                          <FaPlus className={styles.tdContentIcon} onClick={() => handleOnClick(product.productId, 'add')} />
-                        </div>
-                      </td>
-                    </tr> : null
-                ))
-              }
-            </tbody>
-          </table>
-        </div>
-      </div>
-
-      <div className={styles.availableProductsList}>
+    <>
+      <div className={styles.StepTwoContainer}>
+        <FaCircleArrowLeft className={styles.AddIcon} onClick={() => addProductsToReceipt()} />
         <div className={styles.receiptProductsList}>
           <div className={styles.tableContainer}>
             <table className={styles.table}>
               <thead>
-                <th width="10%">Action</th>
+                <th>Action</th>
                 <th>Product Name</th>
                 <th>Labor</th>
                 <th>Price</th>
+                <th>Add product</th>
               </thead>
 
               <tbody>
                 {
-                  AvailableProducts && AvailableProducts.map((product) => (
+                  Products.map((product) => (
                     product.count !== 0 ?
                       <tr>
                         <td>
                           <div className={styles.td}>
-                            <input checked={SelectedProducts.some((selected) => selected.id === product.id)} onClick={(e) => selectProduct(e)} className={styles.selectCheckBox} type="checkbox" id={product.id} />
+                            <IoCloseCircleSharp onClick={deleteProduct} id={product.productId || product.id} style={{ color: 'red', cursor: 'pointer', width: '20px', height: '20px' }} />
                           </div>
                         </td>
                         <td>
@@ -177,7 +147,13 @@ export const FormStepTwo = ({ Receipt }) => {
                             ${product.costPerUnit}
                           </div>
                         </td>
-
+                        <td>
+                          <div className={styles.tdContent}>
+                            <FaMinus className={styles.tdContentIcon} onClick={() => handleOnClick(product?.productId, 'delete')} />
+                            <input type="text" /* readOnly={!edit} */ value={product.count} name="" id="" className={styles.inputTable} />
+                            <FaPlus className={styles.tdContentIcon} onClick={() => handleOnClick(product.productId, 'add')} />
+                          </div>
+                        </td>
                       </tr> : null
                   ))
                 }
@@ -185,7 +161,62 @@ export const FormStepTwo = ({ Receipt }) => {
             </table>
           </div>
         </div>
+
+        <div className={styles.availableProductsList}>
+          <div className={styles.receiptProductsList}>
+            <div className={styles.tableContainer}>
+              <table className={styles.table}>
+                <thead>
+                  <th width="10%">Action</th>
+                  <th>Product Name</th>
+                  <th>Labor</th>
+                  <th>Price</th>
+                </thead>
+
+                <tbody>
+                  {
+                    AvailableProducts && AvailableProducts.map((product) => (
+                      product.count !== 0 ?
+                        <tr>
+                          <td>
+                            <div className={styles.td}>
+                              <input checked={SelectedProducts.some((selected) => selected.id === product.id)} onClick={(e) => selectProduct(e)} className={styles.selectCheckBox} disabled={isProductExists(product)} type="checkbox" id={product.id} />
+                            </div>
+                          </td>
+                          <td>
+                            <div className={styles.tdContent}>
+                              {product.title}
+                            </div>
+                          </td>
+                          <td>
+                            <div className={styles.tdContent}>
+                              {product.effort} hours
+                            </div>
+                          </td>
+                          <td>
+                            <div className={styles.tdContent}>
+                              ${product.costPerUnit}
+                            </div>
+                          </td>
+
+                        </tr> : null
+                    ))
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
       </div>
-    </div>
+      <div className={styles.buttonsContainer}>
+        <button className={styles.formButton} onClick={() => changeStep(0)}>
+          Prev
+        </button>
+        <button className={styles.formButton} onClick={() => changeStep(2)}>
+          Next
+        </button>
+      </div>
+    </>
   )
 }
